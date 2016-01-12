0b7d1553c43fb31ce33285355794d5f688a5047d
m4ker <makerwang@gmail.com>
2016-01-13 00:06:22 +0800

    C和V基本结构

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,17 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all;
+    team_id = params[:team_id]
+    limit   = 15
+
+    @result = Hash.new
+    events = Event.where("team_id = ?", team_id).order(created_at: :desc).limit(limit);
+    for event in events
+      time = event.created_at
+      if @result.has_key?(time)
+        @result[time].push(event)
+      else
+        @result[time] = [event]
+      end
+    end
   end
 end
